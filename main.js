4a14f1fe1a062147c6d0b95d2f2fca09af16ce2f
shreemaahw <105507038+shreemaahw@users.noreply.github.com>
2022-05-14 21:10:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,50 @@
 (() => {
-    const groupContainer = document.getElementsByTagName('group-container')[0];
+    const groupContainer = document.body.getElementsByTagName('group-container')[0];
+    const errorMsgEle = document.body.getElementsByClassName('error-message')[0];
     groupContainer.addEventListener('initialized', () => {
         fetch(`https://shreemaahw.github.io/online/data-file.json?random=${Math.random()}`)
             .then(response => response.json())
             .then(data => {
-                console.log(data)
-                console.log(groupContainer)
+                errorMsgEle.classList.add('d-none');
                 groupContainer.groupData = data;
+            }).catch(e => {
+                errorMsgEle.classList.remove('d-none');
             });
+    });
+    groupContainer.addEventListener('itemClicked', (event) => {
+        const appHeader = document.body.getElementsByTagName('app-header')[0];
+        appHeader.selectedItem = event.detail;
     });
 })();
 
 (() => {
     [
         './components/app-carousel.js',
+        './components/app-header.js',
         './components/group-container.js',
         './components/item-card.js',
         './components/item-group.js',
+        './components/item-order.js',
     ].forEach(path => {
         var s = document.createElement('script');
         s.setAttribute('src', path);
         document.head.appendChild(s);
     });
 })();
+
+showItemOrderRow = () => {
+    const groupContainerRow = document.body.getElementsByClassName('group-container-page')[0];
+    const itemOrderRow = document.body.getElementsByClassName('item-order-page')[0];
+    groupContainerRow.classList.add('d-none');
+    itemOrderRow.classList.remove('d-none');
+}
+
+showGroupContainerRow = () => {
+    const groupContainerRow = document.body.getElementsByClassName('group-container-page')[0];
+    const itemOrderRow = document.body.getElementsByClassName('item-order-page')[0];
+    groupContainerRow.classList.remove('d-none');
+    itemOrderRow.classList.add('d-none');
+}
 
 sendToWhatsapp = () => {
     const userDetails = {
