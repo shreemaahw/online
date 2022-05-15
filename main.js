866d1fa85185283b673848400dd56c99aa25125e
shreemaahw <105507038+shreemaahw@users.noreply.github.com>
2022-05-16 00:02:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,7 @@
 (() => {
-    const groupContainer = document.body.getElementsByTagName('group-container')[0];
-    const errorMsgEle = document.body.getElementsByClassName('error-message')[0];
-    groupContainer.addEventListener('initialized', () => {
-        fetch(`https://shreemaahw.github.io/online/data-file.json?random=${Math.random()}`)
-            .then(response => response.json())
-            .then(data => {
-                errorMsgEle.classList.add('d-none');
-                groupContainer.groupData = data;
-            }).catch(e => {
-                errorMsgEle.classList.remove('d-none');
-            });
-    });
-    groupContainer.addEventListener('itemClicked', (event) => {
-        const appHeader = document.body.getElementsByTagName('app-header')[0];
-        appHeader.selectedItem = event.detail;
-    });
-})();
-
-(() => {
     [
+        './components/app-root.js',
+        './components/home-page/home-page.js',
         './components/home-page/app-carousel.js',
         './components/home-page/app-header.js',
         './components/home-page/group-container.js',
@@ -34,17 +17,3 @@
         document.head.appendChild(s);
     });
 })();
-
-showItemOrderRow = () => {
-    const groupContainerRow = document.body.getElementsByClassName('group-container-page')[0];
-    const itemOrderRow = document.body.getElementsByClassName('order-summary-page')[0];
-    groupContainerRow.classList.add('d-none');
-    itemOrderRow.classList.remove('d-none');
-}
-
-showGroupContainerRow = () => {
-    const groupContainerRow = document.body.getElementsByClassName('group-container-page')[0];
-    const itemOrderRow = document.body.getElementsByClassName('order-summary-page')[0];
-    groupContainerRow.classList.remove('d-none');
-    itemOrderRow.classList.add('d-none');
-}
